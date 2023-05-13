ad01946d4459343726e69c25d5d5d77b884bdb90
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-05-13 17:22:41 -0500

    vocab and vectorizer

---
+++ vectorizer.py
@@ -0,0 +1,15 @@
+@dataclass
+class Vectorizer:
+    vocab: Vocabulary
+
+    @classmethod
+    def from_corpus(cls, corpus, vocab_size):
+        vocab = Vocabulary()
+        for token in corpus:
+            vocab.add(token)
+        vocab_subset = vocab.get_topk_subset(vocab_size)
+        vocab_subset.shuffle()
+        return cls(vocab_subset)
+
+    def vectorize(self, corpus):
+        return [self.vocab[token] for token in corpus]

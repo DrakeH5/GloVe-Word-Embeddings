147b1e489a1ed28b84b9a3c19d48dac517b98716
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-05-19 17:18:42 -0500

    vocabulary

--- train.py
+++ train.py
@@ -1,3 +1,29 @@
+import pickle
+
+#from torchtext.data import get_tokenizer
+#tokenizer = get_tokenizer("basic_english")
+
+from vocabulary import countCooccuringPairs, shuffleVocab
+
+def loadTrainingData():
+    print("Loading training data...")
+    with open("./traningData.pickle", "rb") as f:
+        return pickle.load(f)
+
+
+#def train():
+    
+
 
 def main():
     print("Program Opened")
+    corpus = loadTrainingData()
+    #tokenizer(corpus)
+    vocabTokens, vocabCount, token2Index = countCooccuringPairs(corpus[:100])
+    print(vocabTokens[0], vocabCount[0], token2Index[vocabTokens[0]])
+    vocabTokens, vocabCount, token2Index = shuffleVocab(vocabTokens, vocabCount, token2Index)
+    print(vocabTokens[0], vocabCount[0], token2Index[vocabTokens[0]])
+    
+
+
+main()
---
+++ vocabulary.py
@@ -0,0 +1,32 @@
+import random
+
+def countCooccuringPairs(corpus):
+    print("Creating Vocab...")
+    tokens = []
+    convertTokenToIndex = {}
+    nmbOfOccurences = []
+    for token in corpus:
+        if token not in tokens:
+            tokens.append(token)
+            convertTokenToIndex[token] = len(convertTokenToIndex)
+            nmbOfOccurences.append(0)
+        nmbOfOccurences[convertTokenToIndex[token]]+=1
+    return tokens, nmbOfOccurences, convertTokenToIndex 
+
+
+def shuffleVocab(vocabTokens, vocabCount, token2Index):
+    print("Shuffling...")
+    NEWtokens = []
+    for i in range(len(vocabTokens)):
+        NEWtokens.append("")
+    NEWconvertTokenToIndex = {}
+    NEWnmbOfOccurences = []
+    for i in range(len(vocabCount)):
+        NEWnmbOfOccurences.append(0)
+    new_index = [_ for _ in range(len(vocabTokens))]
+    random.shuffle(new_index)
+    for i in range(len(vocabTokens)):
+        NEWtokens[i] = vocabTokens[new_index[i]]
+        NEWconvertTokenToIndex[NEWtokens[i]] = i
+        NEWnmbOfOccurences[i] = vocabCount[token2Index[vocabTokens[new_index[i]]]]
+    return NEWtokens, NEWnmbOfOccurences, NEWconvertTokenToIndex 

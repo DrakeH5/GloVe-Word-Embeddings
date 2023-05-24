302b1e2a7f455740fe2afc8b141f1cae201c702e
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-05-24 14:44:28 -0500

    shuffling

--- openCooccurence.py
+++ openCooccurence.py
@@ -1,9 +1,11 @@
 import h5py
 import torch
+import random
 
 def openCooccurence():
     with h5py.File("./cooccurrenceEntries\cooccurrence.hdf5", "r") as f:
         dataset = f["cooccurence"][()]  # returns as a numpy array
+        random.shuffle(dataset)
         outputData = [
             torch.from_numpy(dataset[:,:2]).long(),
             torch.from_numpy(dataset[:,2]).float()
